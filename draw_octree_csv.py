eaad29294376f26c9c87d85a9921043007e7e4b6
alwynster.burger <alwyn.burger@outlook.com>
2014-10-29 21:26:41 +0000

    work

--- draw_octree_csv.py
+++ draw_octree_csv.py
@@ -5,14 +5,17 @@ import numpy as np
 import cv2
 import math
 
-if len(sys.argv) != 2 and len(sys.argv) != 3:
-    print "USAGE: ./draw_octree_csv octree.csv [height]"
+if len(sys.argv) != 3 and len(sys.argv) != 4 and len(sys.argv) != 5:
+    print "USAGE: ./draw_octree_csv octree.csv image [height] [thickness]"
     sys.exit()
     
 minx = maxx = miny = maxy = res = 0.0
 height = None
-if len(sys.argv) == 3:
-    height = float(sys.argv[2])
+thickness = None
+if len(sys.argv) > 3:
+    height = float(sys.argv[3])
+if len(sys.argv) > 4:
+    thickness = float(sys.argv[4])
     
 filename = sys.argv[1]
 f = open(filename)
@@ -26,8 +29,12 @@ for line in f:
         add = True
         dat = np.array(spl).astype('float')
         if height is not None:
-            if abs(dat[2] - height) > dat[3]:
-                add = False
+            if thickness is None:
+                if abs(dat[2] - height) > dat[3]:
+                    add = False
+            else:
+                if abs(dat[2] - height) > thickness:
+                    add = False
             
         if add:
             data.append(dat)
@@ -40,12 +47,21 @@ for dat in data:
     y -= miny
     x /= res
     y /= res
-    x, y = int(x), int(y)
+#     x, y = int(x, int(y)
     
     # if img[y,x] < val:
 #     xp = math.exp(val)
-    img[y:y+(size/res), x:x+(size/res)] = val # xp / (1 + xp)
+    sz = (size/res)
+    img[round(y-sz/2):round(y+sz/2), round(x-sz/2):round(x+sz/2)] += val # xp / (1 + xp)
+#     img[y-sz/2:y+sz/2, x-sz/2:x+sz/2] = xp / (1 + xp)
+#     img[y, x] = xp / (1 + xp)
         
 xp = np.exp(img)
-cv2.imshow('img', xp / (1+xp))
-cv2.waitKey()
+img = xp / (1+xp)
+print 'Writing image', sys.argv[2]
+imgout = np.array(img)
+imgout *= 255
+imgout = imgout.astype('uint8')
+cv2.imwrite(sys.argv[2], imgout)
+# cv2.imshow('img', xp / (1+xp))
+# cv2.waitKey()
