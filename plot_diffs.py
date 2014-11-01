1b5739c7f73d90102983acb2afcc096f760198d2
AJ Burger (home) <alwynster.burger@gmail.com>
2014-11-01 15:21:58 +0200

    changed diffs plot

--- plot_diffs.py
+++ plot_diffs.py
@@ -7,13 +7,18 @@ Created on 01 Nov 2014
 import matplotlib.pyplot as pp
 import numpy as np
 import sys
+import images
 
-if len(sys.argv) != 2:
-    print "USAGE: plot_diffs.py diffs.npz"
+if len(sys.argv) != 3:
+    print "USAGE: plot_diffs.py diffs.npz outfile"
     sys.exit()
     
 data = np.load(sys.argv[1])
 diffs = data['diffs']
 
-pp.hist(diffs)
-pp.show()
+count, x, patches = pp.hist(diffs)
+
+print x
+print count
+
+images.save_latex(x[:-1], count, None, sys.argv[2], ylim=None, bar=True)
